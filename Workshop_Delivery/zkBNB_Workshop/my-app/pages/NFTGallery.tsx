12ccbf7ff8f2eaa9d8301a725c380cce81969d4a
RyanFcr <3200103626@zju.edu.cn>
2023-04-15 18:33:33 +0800

    my homework

---
+++ Workshop_Delivery/zkBNB_Workshop/my-app/pages/NFTGallery.tsx
@@ -0,0 +1,20 @@
+//@ts-nocheck
+import React from "react";
+import NFTCard from "./NFTCard";
+
+const NFTGallery = ({ props }) => {
+  const nfts = JSON.parse(props);
+  console.log("NFT objects:");
+  console.log(nfts);
+  return (
+    <div className="container mx-auto pt-1 mb-4">
+      <div className="row row-cols-lg-4">
+        {nfts.map((token) => (
+          <NFTCard key={token.index} nft={token} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default NFTGallery;
