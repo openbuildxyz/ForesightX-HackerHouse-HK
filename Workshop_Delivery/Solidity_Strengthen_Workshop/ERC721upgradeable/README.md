f47884bc92a51c44fc30d0134b72d0924ec02cba
rgyang <https://rgyang@bitbucket.pinghu.tech>
2023-04-15 21:53:47 +0800

    solidity strengthen workshop

---
+++ Workshop_Delivery/Solidity_Strengthen_Workshop/ERC721upgradeable/README.md
@@ -0,0 +1,6 @@
+deployed on sepolia testnet
+https://sepolia.etherscan.io/address/0xfd32dC187D0d6f267e9FA3f5E48F9ADf507435D4#code
+
+deploy and upgrade tx hash:
+https://sepolia.etherscan.io/tx/0xac64742b017e6f8ce3ab906c03b1ab4b79ea9cb69970e4e87444191dde469b62
+https://sepolia.etherscan.io/tx/0xd354ba0714bc0493b9eef3218d40d8193aef000a14203086827f339a39a36fef
---
+++ Workshop_Delivery/Solidity_Strengthen_Workshop/ERC721upgradeable/scripts/deploy.ts
@@ -0,0 +1,30 @@
+import { ethers,upgrades } from "hardhat";
+
+async function main() {
+
+  // Deploying
+  const MyPunksUpgradeable = await ethers.getContractFactory("MyPunksUpgradeable");
+  const myPunksUpgradeable = await upgrades.deployProxy(MyPunksUpgradeable);
+
+  await myPunksUpgradeable.deployed();
+
+  console.log(
+    `Deployed MyPunks to :${myPunksUpgradeable.address} with ${myPunksUpgradeable.deployTransaction.hash}` 
+  );
+
+    // Upgrading
+    const MyPunksUpgradeableV2 = await ethers.getContractFactory("MyPunksUpgradeableV2");
+    const myPunksUpgradeableV2 = await upgrades.upgradeProxy(myPunksUpgradeable.address, MyPunksUpgradeableV2);
+
+    console.log(
+      `Deployed MyPunks to :${myPunksUpgradeableV2.address} with ${myPunksUpgradeableV2.deployTransaction.hash}` 
+    );
+
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
